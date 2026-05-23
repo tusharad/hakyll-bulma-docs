ae324e346701e6c3a7e42777d024bb87dff11c9a
Tushar Adhatrao <tusharadhatrao@gmail.com>
2026-05-23 18:07:13 +0000

    feat: add search functionality and update sidebar with overview and AWS services links

--- site.hs
+++ site.hs
@@ -5,8 +5,56 @@ import Data.Maybe (fromMaybe)
 import Data.Monoid (mappend)
 import Hakyll
 
+sectionContext :: Context String
+sectionContext =
+    listField "overview" defaultContext (loadAll "posts/01-overview/*") `mappend`
+    listField "aws-services" defaultContext (loadAll "posts/02-aws-services/*")
+
+pageContext :: Context String
+pageContext = sectionContext `mappend` defaultContext
+
+stripHtml :: String -> String
+stripHtml = unwords . words . go False
+  where
+    go _ [] = []
+    go inTag (c:cs)
+        | c == '<' = go True cs
+        | c == '>' = go False cs
+        | inTag = go True cs
+        | otherwise = c : go False cs
+
+buildSearchIndex :: Compiler (Item String)
+buildSearchIndex = do
+    indexedItems <- loadAllSnapshots "posts/**" "content"
+    homeItems <- loadAllSnapshots "index.html" "content"
+    let searchItems = indexedItems ++ homeItems
+    entries <- mapM toSearchEntry searchItems
+    makeItem $ "window.DOC_SEARCH_INDEX = [" ++ intercalate "," (map toJavascriptObject entries) ++ "];"
+  where
+    toSearchEntry :: Item String -> Compiler (String, String, String, String)
+    toSearchEntry item = do
+        let identifier = itemIdentifier item
+        title <- fromMaybe "Untitled" <$> getMetadataField identifier "title"
+        route <- getRoute identifier
+        let url = maybe ("/" ++ toFilePath identifier) (('/' :) . toUrl) route
+            content = stripHtml (itemBody item)
+            summary = take 180 content
+        pure (title, summary, url, content)
+
+    toJavascriptObject :: (String, String, String, String) -> String
+    toJavascriptObject (title, summary, url, content) =
+        "{title:" ++ show title ++
+        ",summary:" ++ show summary ++
+        ",url:" ++ show url ++
+        ",content:" ++ show content ++
+        "}"
+
 main :: IO ()
 main = hakyll $ do
+    create ["search-index.js"] $ do
+        route idRoute
+        compile buildSearchIndex
+
     match "images/*" $ do
         route   idRoute
         compile copyFileCompiler
@@ -22,29 +70,27 @@ main = hakyll $ do
     match "posts/01-overview/*" $ do
         route $ setExtension "html"
         compile $ pandocCompiler
+            >>= saveSnapshot "content"
             >>= loadAndApplyTemplate "templates/post.html"    defaultContext
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= loadAndApplyTemplate "templates/default.html" pageContext
             >>= relativizeUrls
 
     match "posts/02-aws-services/*" $ do
         route $ setExtension "html"
         compile $ pandocCompiler
+            >>= saveSnapshot "content"
             >>= loadAndApplyTemplate "templates/post.html"    defaultContext
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= loadAndApplyTemplate "templates/default.html" pageContext
             >>= relativizeUrls
 
     match "index.html" $ do
         route idRoute
         compile $ do
-            overviewPosts <- loadAll "posts/01-overview/*"
-            awsServicesPosts <- loadAll "posts/02-aws-services/*"
-            let indexCtx =
-                    listField "overview" defaultContext (return overviewPosts) `mappend`
-                    listField "aws-services" defaultContext (return awsServicesPosts) `mappend`
-                    defaultContext
+            let indexCtx = pageContext
 
             getResourceBody
                 >>= applyAsTemplate indexCtx
+                >>= saveSnapshot "content"
                 >>= loadAndApplyTemplate "templates/default.html" indexCtx
                 >>= relativizeUrls
 
